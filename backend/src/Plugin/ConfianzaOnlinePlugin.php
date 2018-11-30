6405547bfe8a74fdf05334acca93e46c67d6f2e9
Alejandro Sánchez <contacto@alsanchez.es>
2018-11-30 12:24:47 +0100

    Add Confianza Online scanner

---
+++ backend/src/Plugin/ConfianzaOnlinePlugin.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Ontic\Iuris\Plugin;
+
+use Facebook\WebDriver\Exception\NoSuchElementException;
+use Facebook\WebDriver\WebDriverBy;
+use Ontic\Iuris\Interfaces\IPlugin;
+use Ontic\Iuris\Model\AnalysisDetail;
+use Ontic\Iuris\Model\AnalysisRequest;
+
+class ConfianzaOnlinePlugin implements IPlugin
+{
+    /**
+     * @return string
+     */
+    function getName()
+    {
+        return 'confianza_online';
+    }
+
+    /**
+     * @param AnalysisRequest $request
+     * @return AnalysisDetail
+     */
+    function analyze(AnalysisRequest $request)
+    {
+        try
+        {
+            $xpath = "//a[starts-with(@href, 'https://www.confianzaonline.es/empresas/')]";
+            $request->getWebdriver()->findElement(WebDriverBy::xpath($xpath));
+            $score = 100;
+            $message = 'Website enrolled in the Confianza Online program.';
+        } 
+        /** @noinspection PhpRedundantCatchClauseInspection */ 
+        catch(NoSuchElementException $ignored)
+        {
+            $score = 0;
+            $message = 'Enroll in the Confianza Online program to increase your website reputation.';
+        }
+        
+        return new AnalysisDetail(
+            $this->getName(),
+            0,
+            $score,
+            $message
+        );
+    }
+}
