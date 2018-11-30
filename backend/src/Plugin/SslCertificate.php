e3ac86ba3de0505318428a1d694b2a84284ad161
Ricardo <ricardo@ontic.es>
2018-11-30 11:30:44 +0100

    SSL Certificate plugin

---
+++ backend/src/Plugin/SslCertificate.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Ontic\Iuris\Plugin;
+
+use Ontic\Iuris\Interfaces\IPlugin;
+use Ontic\Iuris\Model\AnalysisDetail;
+use Ontic\Iuris\Model\AnalysisRequest;
+use Ontic\Iuris\Model\Flag;
+
+class SslCertificate implements IPlugin
+{
+    /**
+     * @param AnalysisRequest $request
+     * @return AnalysisDetail
+     */
+    function analyze(AnalysisRequest $request)
+    {
+    $score=0;
+    $url = $request->getWebdriver()->getCurrentURL()
+    $orignal_parse = parse_url($url, PHP_URL_HOST);
+    $get = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE)));
+
+    set_error_handler(function ($err_severity, $err_msg, $err_file, $err_line, array $err_context)
+	{
+        throw new ErrorException( $err_msg, 0, $err_severity, $err_file, $err_line );
+	}, E_WARNING);
+
+    try {
+	$Result = $read = stream_socket_client("ssl://".$orignal_parse.":443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $get);
+        $score=100;
+	$message="SSL Certificate correct and up to date.";
+	} catch (Exception $e) {
+    if (strpos($e->getMessage(),"SSL Certificate verify failed")===false) 
+	{
+	$score=0;
+	$message="SSL Certificate failed.\nCheck your SSL installation in ".$url;
+	}
+    else 
+	{ //Certificado invalido;
+	$message="SSL Certificate invalid or outdate\n Please install a valid certificate and up to date for domain ".$url;
+	$score=0;
+	}
+}
+restore_error_handler();
+
+        return new AnalysisDetail(
+            $this->getName(), 
+            Flag::Scorable, 
+            $score, 
+            $message'
+        );
+    }
+
+    /**
+     * @return string
+     */
+    function getName()
+    {
+        return 'ssl_certificate';
+    }
+}
