51f8e0e7766569e1e7e2ceeace1311773201abbe
Alejandro Sánchez <contacto@alsanchez.es>
2018-11-30 12:07:52 +0100

    Refactor SSL plugin

--- backend/src/Plugin/SslCertificate.php
+++ backend/src/Plugin/SslCertificate.php
@@ -15,39 +15,41 @@ class SslCertificate implements IPlugin
      */
     function analyze(AnalysisRequest $request)
     {
-    $score=0;
-    $url = $request->getWebdriver()->getCurrentURL()
-    $orignal_parse = parse_url($url, PHP_URL_HOST);
-    $get = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE)));
+        $url = $request->getWebdriver()->getCurrentURL();
+        $orignal_parse = parse_url($url, PHP_URL_HOST);
+        $get = stream_context_create(['ssl' => ['capture_peer_cert' => true]]);
 
-    set_error_handler(function ($err_severity, $err_msg, $err_file, $err_line, array $err_context)
-	{
-        throw new ErrorException( $err_msg, 0, $err_severity, $err_file, $err_line );
-	}, E_WARNING);
+        set_error_handler(function ($err_severity, $err_msg, $err_file, $err_line) {
+            throw new \ErrorException($err_msg, 0, $err_severity, $err_file, $err_line);
+        }, E_WARNING);
 
-    try {
-	$Result = $read = stream_socket_client("ssl://".$orignal_parse.":443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $get);
-        $score=100;
-	$message="SSL Certificate correct and up to date.";
-	} catch (Exception $e) {
-    if (strpos($e->getMessage(),"SSL Certificate verify failed")===false) 
-	{
-	$score=0;
-	$message="SSL Certificate failed.\nCheck your SSL installation in ".$url;
-	}
-    else 
-	{ //Certificado invalido;
-	$message="SSL Certificate invalid or outdate\n Please install a valid certificate and up to date for domain ".$url;
-	$score=0;
-	}
-}
-restore_error_handler();
+        try
+        {
+            stream_socket_client('ssl://' . $orignal_parse . ':443', $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $get);
+            $score = 100;
+            $message = 'SSL Certificate correct and up to date.';
+        } 
+        catch (\Exception $e)
+        {
+            if (strpos($e->getMessage(), 'SSL Certificate verify failed') === false)
+            {
+                $score = 0;
+                $message = "SSL Certificate failed.\nCheck your SSL installation in " . $url;
+            } 
+            else
+            { //Certificado invalido;
+                $message = "SSL Certificate invalid or outdate\n Please install a valid certificate and up to date for domain " . $url;
+                $score = 0;
+            }
+        }
+        
+        restore_error_handler();
 
         return new AnalysisDetail(
-            $this->getName(), 
-            Flag::Scorable, 
-            $score, 
-            $message'
+            $this->getName(),
+            Flag::Scorable,
+            $score,
+            $message
         );
     }
 
