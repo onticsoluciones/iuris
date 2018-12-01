c4500785d7fd924b11766f86886e1355f5f50a51
ricardo@ontic.es <proyect0s>
2018-12-01 12:52:20 +0100

    Renderizado ODT

--- backend/src/Service/Renderer/OdtRenderer.php
+++ backend/src/Service/Renderer/OdtRenderer.php
@@ -8,6 +8,17 @@ use PhpOffice\PhpWord\PhpWord;
 
 class OdtRenderer
 {
+    private $rootDir;
+
+    /**
+     * @param string $rootDir
+     */
+    public function __construct($rootDir)
+    {
+        $this->rootDir = $rootDir;
+    }
+
+
     /**
      * @param Analysis $analysis
      * @return string
@@ -16,36 +27,91 @@ class OdtRenderer
     public function getOdt(Analysis $analysis)
     {
         $document = new PhpWord();
-        
+        $document->addFontStyle('titleOK',array('bold'=>true,'size'=>14, 'color'=>'009933','name'=>'Helvetica'));
+        $document->addFontStyle('mesOK',array('bold'=>true,'size'=>12, 'color'=>'009933','name'=>'DejaVu Sans, sans-serif'));
+
+        $document->addFontStyle('titleFAIL',array('bold'=>true,'size'=>14, 'color'=>'b30000','name'=>'Helvetica'));
+        $document->addFontStyle('mesFAIL',array('bold'=>true,'size'=>12, 'color'=>'b30000','name'=>'DejaVu Sans, sans-serif'));
+
+        $document->addFontStyle('titleWARN',array('bold'=>true,'size'=>14, 'color'=>'ff9900','name'=>'Helvetica'));
+        $document->addFontStyle('mesWARN',array('bold'=>true,'size'=>12, 'color'=>'ff9900','name'=>'DejaVu Sans, sans-serif'));
+
+        $document->addFontStyle('info',array('bold'=>false,'size'=>12 , 'color'=>'000000','name'=>'DejaVu Sans, sans-serif'));
+
+        //Title
+        $title = $document->addSection();
+        $title->addImage($this->rootDir.'/assets/logo_iuris.png');
+        $title->addText("Informe Técnico de ".$analysis->getUrl(),array('name'=>'Courier','bold'=>true, 'color'=>'000000', 'size'=>30),array('align'=>'center', 'spaceAfter'=>100));
+        //$title->addText('______________________________________________________________________________________________'    );
+        $title->addTextBreak('1');
+
         foreach($analysis->getDetails() as $detail)
         {
             $section = $document->addSection();
-            
+
             // Title
-            $titleStyle = new \PhpOffice\PhpWord\Style\Font();
-            $titleStyle->setName('DejaVu Sans, sans-serif');
-            $titleStyle->setBold(true);
-            $titleText = sprintf('%s - %s', $detail->getAnalyzer(), $detail->getScore());
-            $titleElement = $section->addText($titleText);
-            $titleElement->setFontStyle($titleStyle);
-            
+            $table=$section->addTable();
+            $table->addRow();
+            if ($detail->getScore() == 100) {
+                $table->addCell()->addText("Verificación: ".$detail->getAnalyzer(),'titleOK');
+                $table->addCell()->addText("Puntuación: ".$detail->getScore(),'titleOK');
+            }
+            elseif ($detail->getScore() >= 50 && $detail->getScore() < 100 ) {
+                $table->addCell()->addText("Verificación: ".$detail->getAnalyzer(),'titleWARN');
+                $table->addCell()->addText("Puntuación: ".$detail->getScore(),'titleWARN');
+            }
+            else {
+                $table->addCell()->addText("Verificación: ".$detail->getAnalyzer(),'titleFAIL');
+                $table->addCell()->addText("Puntuación: ".$detail->getScore(),'titleFAIL');            }
             // Message
             foreach(explode("\n", $detail->getMessage()) as $line)
             {
-                $messageStyle = new \PhpOffice\PhpWord\Style\Font();
-                $messageStyle->setName('DejaVu Sans, sans-serif');
-                $textElement = $section->addText(htmlspecialchars($line, ENT_XML1));
-                $textElement->setFontStyle($messageStyle);
+                $table->addRow();
+                if ($detail->getScore() == 100){
+                    $cell = $table->addCell();
+                    $cell->addText($line)->setFontStyle('titleOK');
+                    $cell->getStyle()->setGridSpan(2);
+                }
+                elseif(strpos($line,'⚠')!== FALSE){
+                    // $section->addText('   * Información del Fallo','info');
+                    $cell = $table->addCell();
+                    $cell->addText($line)->setFontStyle('titleWARN');
+                    $cell->getStyle()->setGridSpan(2);
+                }
+                else{
+                    //$section->addText('   * Información del Fallo','info');
+                    $cell = $table->addCell();
+                    $cell-> addText($line)->setFontStyle('titleFAIL');
+                    $cell->getStyle()->setGridSpan(2);
+                }
             }
+            //$section->addText('______________________________________________________________________________________________'    );
+            $section->addTextBreak('1');
         }
-        
+
+            $section->addText('______________________________________________________________________________________________');
+            $section->addTextBreak('3');
+
+            if ($analysis->getGlobalScore() == 100) {
+                $section->addText("Puntuación Total: " . round($analysis->getGlobalScore()), 'titleOK');
+                //Añadir SELLO
+                $section->addImage($this->rootDir . '/assets/logocompliance.jpg');
+
+
+            } elseif ($analysis->getGlobalScore() >= 50 && $detail->getScore() < 100) {
+                $section->addText("Puntuación Total: " . round($analysis->getGlobalScore()), 'titleWARN');
+
+            } else {
+                $section->addText("Puntuación Total: " . round($analysis->getGlobalScore()), 'titleFAIL');
+            }
+
         $tmpFile = tempnam(sys_get_temp_dir(), 'iuris');
         $writer = IOFactory::createWriter($document, 'ODText');
         $writer->save($tmpFile);
         $output = file_get_contents($tmpFile);
         unlink($tmpFile);
-        
+
         return $output;
     }
-    
+
 }
